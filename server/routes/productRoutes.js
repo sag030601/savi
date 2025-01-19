06a0785663080c06b78de778ed6a808a9cf156d6
sag030601 <121371237+sag030601@users.noreply.github.com>
2025-01-19 10:13:39 +0530

    Delete server directory

--- server/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getProducts, createProduct } = require('../controllers/productController');
-const { protect, admin } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.get('/', getProducts);
-router.post('/', protect, admin, createProduct);
-
-module.exports = router;
